dd84fa0ef158e750cc19fbc18a355d2a97ec0f98
Vishal <vishal.g@wingstechsolutions.com>
2022-03-02 18:40:17 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App with CI CD"/>
+  <HelloWorld msg="Welcome to Your Vue.js App with CI"/>
 </template>
 
 <script>
